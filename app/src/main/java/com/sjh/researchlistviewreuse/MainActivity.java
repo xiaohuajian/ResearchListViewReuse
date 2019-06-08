95561f5b3188f7668103d7b8878cdd67e3de43a3
jianhua.shi <jianhua.shi@pendanaan.com>
2019-06-08 20:50:09 +0800

    Initial commit

---
+++ app/src/main/java/com/sjh/researchlistviewreuse/MainActivity.java
@@ -0,0 +1,27 @@
+package com.sjh.researchlistviewreuse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
